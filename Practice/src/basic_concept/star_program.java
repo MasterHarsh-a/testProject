8c6b3d8ce2cea331f11e67e8326f31ae6fa8f196
AVITA <AVITA@IT-PC>
2021-06-02 23:35:05 +0530

    right angle

---
+++ Practice/src/basic_concept/star_program.java
@@ -0,0 +1,20 @@
+package basic_concept;
+
+public class star_program {
+	
+	
+	
+		public static void main(String[] arg){
+			for(int i=0; i<6;i++) {
+				for(int j = 0; j<i;j++) {
+					System.out.print("*");
+				}
+			//
+				System.out.println();
+			
+		}
+		
+	}
+
+}
+
