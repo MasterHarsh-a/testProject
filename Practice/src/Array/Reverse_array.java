8967a92eeb601e15d2fdb91291cc0d3c46c8517f
AVITA <AVITA@IT-PC>
2021-06-04 14:59:57 +0530

    Reverse element of Array

---
+++ Practice/src/Array/Reverse_array.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Reverse_array {
+
+	public static void main(String[] args) {
+		// TODO 1> Create a array
+		// 2> Use Collection.reverse
+		//3>print reverse array
+		Integer value[]= {1, 2, 3, 4, 5, 6, 7 ,8 ,9,0};
+		System.out.println("Before Reversing :" +Arrays.toString(value));
+		//Use of Collection.Reverse
+		Collections.reverse(Arrays.asList(value));
+		System.out.println("After Reverse:" + Arrays.toString(value));
+
+	}
+
+}
