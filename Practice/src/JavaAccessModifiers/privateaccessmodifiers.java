470245f5295aee8b98dbb40d2bc8ff97a7bacc51
AVITA <AVITA@IT-PC>
2021-06-10 22:45:20 +0530

    Private Access Modifier

--- Practice/src/JavaAccessModifiers/privateaccessmodifiers.java
+++ Practice/src/JavaAccessModifiers/privateaccessmodifiers.java
@@ -2,9 +2,15 @@ package JavaAccessModifiers;
 
 public class privateaccessmodifiers {
 
+	private int name;// private variable
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+   privateaccessmodifiers pam = new privateaccessmodifiers();
+     pam.name;
+     /**
+      * we can't access methods and variables outside of the class,
+      * when we declare private 
+      */
 	}
 
 }
