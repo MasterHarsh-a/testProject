06ffc5b94af2a34ee4d004d00bed4c9413060118
AVITA <AVITA@IT-PC>
2021-06-10 23:01:40 +0530

    Protected Access Modifier

---
+++ Practice/src/JavaAccessModifiers/protectedaccessmodifier.java
@@ -0,0 +1,16 @@
+package JavaAccessModifiers;
+
+public class protectedaccessmodifier {
+	protected void show() {
+		System.out.println("MasterHarsh-a");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		protectedaccessmodifier s = new protectedaccessmodifier();
+		s.show();
+		
+
+	}
+
+}
