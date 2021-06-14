a60d65b021920f0cd36ab2c1279942c88e3d61bc
MasterHarsh-a <82604748+MasterHarsh-a@users.noreply.github.com>
2021-06-14 19:06:43 +0530

    Update sortanarray.java
    There is an error that happens if we repeat the same number.

--- Practice/src/Array/sortanarray.java
+++ Practice/src/Array/sortanarray.java
@@ -1,19 +1,19 @@
 package Array;
 
 public class sortanarray {
-	static int i;
+	
 
 	void selectionsorrt(int arr[])
 	{
 		int n = arr.length;
 	
 		//for is using to go to every element index
-	for( i=0; i<n-1; i++) {
+	for(int i=0; i<n-1; i++) {
 		
 		int min_index= i;
 		//for is using the minimum element in unsorted element 
 		for(int j=i+1; j<n; j++) {
-			arr[j]=arr[min_index];
+			if(arr[j]<arr[min_index])
 			min_index=j;
 		//swap the minimum element with the first element 	
 			int  min_element =arr[min_index];
@@ -37,8 +37,9 @@ public class sortanarray {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[] = {2,3,5,6,1,4,7};
-		int n = arr.length;
+		int arr[] = {2,3,5,6,1,9,7};
+			
+		
 		sortanarray ob = new sortanarray();
 		ob.selectionsorrt(arr);
 		System.out.println("sorted array");
