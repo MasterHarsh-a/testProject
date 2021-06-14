9050220b56234ab9814a9231fde320e1c04170bb
MasterHarsh-a <82604748+MasterHarsh-a@users.noreply.github.com>
2021-06-14 21:23:40 +0530

    Update sortanarray.java

--- Practice/src/Array/sortanarray.java
+++ Practice/src/Array/sortanarray.java
@@ -1,20 +1,20 @@
 package Array;
 
 public class sortanarray {
-	
+
 
 	void selectionsorrt(int arr[])
 	{
 		int n = arr.length;
 	
 		//for is using to go to every element index
-	for(int i=0; i<n-1; i++) {
+	for(int  i=0; i<n-1; i++) {
 		
 		int min_index= i;
 		//for is using the minimum element in unsorted element 
 		for(int j=i+1; j<n; j++) {
-			if(arr[j]<arr[min_index])
-			min_index=j;
+			if(arr[j]<arr[min_index]) 
+				min_index=j;
 		//swap the minimum element with the first element 	
 			int  min_element =arr[min_index];
 			arr[min_index] = arr[i];
@@ -25,8 +25,43 @@ public class sortanarray {
 			
 			
 		}
+		
 	}
 	
+	}
+	// here we can sort by bubble sort
+	void bubblesort(int arr[])
+	{
+		int n = arr.length;
+		
+		for(int i =0; i<n-1;i++)
+		{
+			 boolean flag = false;
+			for(int j=0;j <n-1-i; j++)
+			{
+				int min_index = j;
+				if(arr[j]<arr[min_index])
+				{
+					int temp =arr[min_index];
+					arr[min_index] = arr[i];
+					arr[i]= temp;
+					flag = true;
+				}
+				
+				
+			}
+			if(flag == false) 
+				break;
+				
+			
+		}
+	}
+	void printbubblesort(int arr[])
+	{
+		int n = arr.length;
+		for(int i =0; i<n;i++)
+			System.out.print(arr[i]+" ");
+		System.out.println();
 	}
 	void printarray(int arr[])
 	{ 
@@ -37,13 +72,15 @@ public class sortanarray {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[] = {2,3,5,6,1,9,7};
-			
-		
+		int arr[] = {2,4,3,5,6,1,9,7};
+		//There is error happen if we repeat same number.
 		sortanarray ob = new sortanarray();
 		ob.selectionsorrt(arr);
 		System.out.println("sorted array");
 		ob.printarray(arr);
+		ob.bubblesort(arr);
+		System.out.println("Sort an array by buuble sort Algorithm");
+		ob.printbubblesort(arr);
 		
 		
 	//
